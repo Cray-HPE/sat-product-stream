d6a92bb9e99dc3b0aa8267809828c1be1d278629
Eli Kamin <eli.kamin@hpe.com>
2021-04-14 17:06:44 -0500

    SAT-899: Product stream updates for SAT CFS install
    * Add manifests/sat.yaml.in, a template for the loftsman manifest.
    * Add sat-cfs-install version (temporary version) to
      component_versions.sh which gets substituted in docker/index.yaml.in
      and manifests/sat.yaml.in.
    * Remove templating of install.sh as there is no more need for variable
      substitution here.
    * Remove cray-product-catalog-update invocation from install.sh as
      adding to the product catalog is now done through the config import.
    
    Test Description: I built this version of the product stream and
    installed it. I then created a CFS session which resulted in SAT
    being updated on all the NCNs.

--- component_versions.sh
+++ component_versions.sh
@@ -2,3 +2,6 @@
 SAT_VERSION=3.5.0-20210325004035_c48be28
 # Always just use a stable release of this since it's an external dependency.
 CPCU_VERSION=0.0.4
+# FIXME: this revision of sat-cfs-install is not the latest master or release version, because it has not been merged yet.
+SAT_CFS_DOCKER_VERSION=1.0.0-20210415205831_9828e74
+SAT_CFS_HELM_VERSION=${SAT_CFS_DOCKER_VERSION/_/+}
