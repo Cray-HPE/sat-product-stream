c2a4a779da732f4be2bdb66eadf8b2184c964f2c
Eli Kamin <eli.kamin@hpe.com>
2021-04-16 14:57:39 -0500

    SAT-909: Update with master versions
    After merging to master, update with new master revisions of
    sat-cfs-install and sat. This also removes a workaround of
    setting the sat-cfs-install repository to 'sat-docker-unstable-local'.

--- component_versions.sh
+++ component_versions.sh
@@ -1,7 +1,6 @@
 # Use a master version if on master branch; use a released version if on release branch
-SAT_VERSION=3.5.0-20210325004035_c48be28
+SAT_VERSION=3.6.0-20210416195541_db59b55
 # Always just use a stable release of this since it's an external dependency.
 CPCU_VERSION=0.0.4
-# FIXME: this revision of sat-cfs-install is not the latest master or release version, because it has not been merged yet.
-SAT_CFS_DOCKER_VERSION=1.0.0-20210415205831_9828e74
+SAT_CFS_DOCKER_VERSION=1.0.0-20210416195535_b4db651
 SAT_CFS_HELM_VERSION=${SAT_CFS_DOCKER_VERSION/_/+}
