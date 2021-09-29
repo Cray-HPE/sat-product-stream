9abcb0cf92f2042ed11bdbed09e79bd773ca6b46
Jack Stanek <jack.stanek@hpe.com>
2021-09-29 12:02:34 -0500

    CRAYSAT-1162: Release version 2.1.16 of the SAT product stream
    Create a 2.1.16 release of the SAT product stream. This version will
    include the following versions of sat and sat-podman:
    
    * sat:         3.7.4
    * sat-podman:  1.4.10
    
    See the changelogs of those repositories for a full description of
    changes.
    
    Test Description:
    Ran `release.sh` and verified that the release distribution contained
    the correct versions.

--- component_versions.sh
+++ component_versions.sh
@@ -1,6 +1,6 @@
 # Use a master version if on master branch; use a released version if on release branch
-SAT_VERSION=3.7.3
-SAT_PODMAN_VERSION=1.4.9-20210909153500_908f460
+SAT_VERSION=3.7.4
+SAT_PODMAN_VERSION=1.4.10-20210930103611_b102663
 # Always just use a stable release of this since it's an external dependency.
 CPCU_VERSION=0.1.3
 SAT_CFS_DOCKER_VERSION=1.0.3
