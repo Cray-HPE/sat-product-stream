c5c250c42bed625a112fabc4d9f5cad83aa10658
Eli Kamin <eli.kamin@hpe.com>
2021-10-28 15:17:42 -0500

    CRAYSAT-1192: Update cray-product-catalog-update and validate schema
    This commit updates the version of cray-product-catalog-update used by
    the SAT product stream to a version that supports schema validation and
    setting an 'active' flag on install. The image is pulled from a
    different repository within arti.dev.cray.com, therefore the release
    script is modified slightly to ensure all the images end up in the same
    `cray/` repository.
    
    Additionally, this commit modifies the call to
    cray-product-catalog-update within the install script, so that the
    VALIDATE_SCHEMA variable is passed to the container so that schema
    validation actually occurs on install.
    
    Finally, the call to cray-product-catalog-update is updated to include
    --user root, which is required to access the Kubernetes configuration
    when running this image via podman.
    
    Test Description: I built the release distribution and inspected the
    layout of the `docker/` subdirectory to make sure all the images were
    under `docker/arti.dev.cray.com/cray/`.
    
    Next, I installed the release distribution and inspected the output
    to ensure that cray-product-catalog-update version 1.2.70 was being
    used, and that it was recognizing the VALIDATE_SCHEMA environment
    variable.
    
    Finally, I inspected the product catalog data after installation, to
    make sure that the new cray-product-catalog-update image works both
    for inserting the data as well as when run as a part of the
    configuration import Kubernetes job, and that the 'active' flag was
    set.

--- component_versions.sh
+++ component_versions.sh
@@ -1,8 +1,7 @@
 # Use a master version if on master branch; use a released version if on release branch
 SAT_VERSION=3.11.0-20211029193657_c204587
 SAT_PODMAN_VERSION=1.5.2-20211029152029_7922a91
-# Always just use a stable release of this since it's an external dependency.
-CPCU_VERSION=0.1.3
+CPCU_VERSION=1.2.70
 SAT_CFS_DOCKER_VERSION=1.0.3-20210907184559_253262a
 SAT_CFS_HELM_VERSION=${SAT_CFS_DOCKER_VERSION/_/+}
 SAT_INSTALL_UTILITY_VERSION=1.3.2-20211029193701_be8a4f3
