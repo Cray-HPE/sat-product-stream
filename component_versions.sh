136bd5dfcbbe75bc60f7da9d04f7366a842f4ec6
Jack Stanek <jack.stanek@hpe.com>
2021-12-14 15:32:56 -0600

    CRAYSAT-1010: Bump component versions to validate product stream build

--- component_versions.sh
+++ component_versions.sh
@@ -1,7 +1,7 @@
 # Use a master version if on master branch; use a released version if on release branch
-SAT_VERSION=3.7.4
-SAT_PODMAN_VERSION=1.4.10-20210930103611_b102663
+SAT_VERSION=3.7.5
+SAT_PODMAN_VERSION=1.4.11-20211215161649_00374c4
 # Always just use a stable release of this since it's an external dependency.
 CPCU_VERSION=0.1.3
-SAT_CFS_DOCKER_VERSION=1.0.3
+SAT_CFS_DOCKER_VERSION=1.0.4
 SAT_CFS_HELM_VERSION=${SAT_CFS_DOCKER_VERSION/_/+}
