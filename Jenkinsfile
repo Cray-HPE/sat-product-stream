29525db87b26691bf197627320594c731a6f6bc7
Eli Kamin <eli.kamin@hpe.com>
2022-06-06 14:20:31 -0500

    CRAYSAT-1451: Update builds for open source
    * Rewrite section of release.sh that moves images into $BUILDDIR/docker/cray
      to allow arbitrary levels of nesting.
    * Copy images, helm charts, and rpms from external locations (sat
      repositories in artifactory).
    * Rewrite Jenkinsfile for external Jenkins
    * Update component versions to versions built in external artifactory.
    
    (cherry picked from commit 97b0890cef7a7ad05f054cf45f0217ae6e920df0)

--- Jenkinsfile
+++
@@ -1,6 +0,0 @@
-@Library('dst-shared@release/sat-2.1.17') _
-
-releaseDistroBuildPipeline {
-    name = "sat"
-    product = "sat"
-}
--- component_versions.sh
+++ component_versions.sh
@@ -1,7 +1,28 @@
-# Use a master version if on master branch; use a released version if on release branch
-SAT_VERSION=3.7.5
-SAT_PODMAN_VERSION=1.4.11-20211215161649_00374c4
+#
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+SAT_VERSION=3.7.6
+SAT_PODMAN_VERSION=1.4.13-1
 # Always just use a stable release of this since it's an external dependency.
 CPCU_VERSION=0.1.3
-SAT_CFS_DOCKER_VERSION=1.0.4
+SAT_CFS_DOCKER_VERSION=1.0.5
 SAT_CFS_HELM_VERSION=${SAT_CFS_DOCKER_VERSION/_/+}
